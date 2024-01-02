845751cb5fbc472b68b296ffb92d35fbbf4cab1c
jGsouzaJr <jgsouzadevweb@gmail.com>
2024-01-02 16:01:55 -0300

    Inicializando Cap 2

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,11 @@
+const frm = document.querySelector('form')
+console.log(frm)
+const resp = document.querySelector('h3')
+console.log(resp)
+
+frm.addEventListener('submit', (e)=>{
+  const nome = frm.inNome.value  
+  resp.innerText = `Olá ${nome}`
+  e.preventDefault()
+})
+
