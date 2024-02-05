fb949235f60b1c70fef20fda3ae16088c5645246
jGsouzaJr <jgsouzadevweb@gmail.com>
2024-02-05 17:09:41 -0300

    cap 6 ex 3

---
+++ cap06/js/ex6_3.js
@@ -0,0 +1,51 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("pre");
+const carros = [];
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const modelo = frm.inNome.value;
+  const preco = Number(frm.inPreco.value);
+  carros.push({ modelo, preco });
+  frm.inNome.value = "";
+  frm.inPreco.value = "";
+  inNome.focus();
+
+  frm.inLista.dispatchEvent(new Event("click"));
+});
+
+frm.inLista.addEventListener("click", () => {
+  if (carros.length == 0) {
+    alert("Não há carros na lista!");
+    frm.inNome.focus()
+    return;
+  }
+  const lista = carros.reduce((acumulador, carro) => 
+    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(
+      40
+    )}\n${lista}`;
+    frm.inNome.focus()
+   
+  
+});
+
+frm.inFiltrar.addEventListener('click',()=>{
+  // let maximo = Number(prompt('Qual o valor máximo que o cliente deseja pagar?'))
+  let maximo = Number(frm.inPreco.value)
+
+  if(maximo == 0 || isNaN(maximo)){
+    return
+  }
+  const carrosFilter = carros.filter(carro=>carro.preco<=maximo)
+  if(carrosFilter.length == 0){
+    alert('Não há carros com preço inferior ou igual ao solicitado')
+    frm.inNome.focus()
+    return
+  }
+  let lista = ''
+  for(const car of carrosFilter){
+    lista += `${car.modelo} - R$: ${car.preco.toFixed(2)}\n`
+  }
+  resp.innerText = `Carros até R$: ${maximo.toFixed(2)}\n${'-'.repeat(40)}\n${lista}`
+})
